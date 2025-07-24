9012d0084d5b5c79f5f6c7045b6e1d74c049fb20
albeany <atabernerovalle@gmail.com>
2025-07-24 16:45:44 +0200

    Inicio del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/zonaPortal/home/home.component';
+import { RegistroComponent } from './components/zonaUsuario/registro/registro.component';
+import { LoginComponent } from './components/zonaUsuario/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+
+    { path: 'Registro', component: RegistroComponent },
+    { path: 'Login', component: LoginComponent },
+];
